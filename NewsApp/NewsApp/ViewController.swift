6324456ff09a8b5b3e17fdeed631760775d431ce
Vidya Murthy <vidya6988@gmail.com>
2017-05-24 10:29:43 +0530

    Initial Commit

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Vidya Murthy on 24/05/17.
+//  Copyright © 2017 Vidya Murthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
